25b443ea84ed1be22e7516b58b9914ba4064ae6b
Javi <monmany1992@gmail.com>
2023-04-09 15:07:26 +0200

    Add uncertain app structure

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Counter title="Learning props" v-bind:start="5"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Counter title="Learning props" v-bind:start="5" />
   <!-- v-bind:start or :start is the same. Here we are passing a prop to the Counter component,
   and we are binding the start property to the value 5. Without binding,
   the start property would be a string value of "5", insted of a number. -->
+  <Uncertain />
 </template>
 
 <script>
-import Counter from './components/Counter.vue'
+import Counter from "./components/Counter.vue";
+import Uncertain from "./components/Uncertain.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Counter
-  }
-}
+    Counter,
+    Uncertain,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Uncertain.vue
@@ -0,0 +1,72 @@
+<template>
+  <img src="" alt="" />
+  <div class="bg-dark"></div>
+  <div class="uncertain-container">
+    <input type="text" placeholder="Ask me a question">
+    <p>Do not forget to write the question mark (?)</p>
+    <div>
+        <h2>Will I be a millionaire?</h2>
+        <h1>Yes, No ... waiting</h1>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {},
+  name: "Uncertain",
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style scoped>
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .uncertain-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+
+</style>
