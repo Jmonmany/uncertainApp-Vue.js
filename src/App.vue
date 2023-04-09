3b6b5c4076c3492a0c6009a0245dd5b82975b05e
Javi <monmany1992@gmail.com>
2023-04-09 12:29:00 +0200

    Add counter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Counter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Counter from './components/Counter.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{counter}} <sup>2</sup> = {{ counter * counter}} </p>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data(){
+        return {
+            counter: 10
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
